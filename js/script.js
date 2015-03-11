0669bebf0c53da23ad9e9ccc38a6eced2fc3c9e1
joshparker <josh@joshuamatthewparker.com>
2015-03-11 15:48:03 -0600

    Fixed hours

--- js/script.js
+++ js/script.js
@@ -80,16 +80,16 @@ function getClock() {
 	// Change the header image based on the time
 	// TODO: Get city local, sunset, and sunset times
 	
-	if (hour >= 6 && hour < 8) {
+	if (hour >= 6 && hour <= 8) {
 		// Dawn
 		headerImage = 'http://lh5.ggpht.com/LeDpxkfCDssG2jwo20Tg01UxnUc4-PZUojwKsPzIQoGJ_CgbXc7KVko8o3nk5zA=w9999-h9999';
-	} else if (hour >= 8 && hour < 15) {
+	} else if (hour >= 9 && hour <= 17) {
 		// Day
 		headerImage = 'http://lh5.ggpht.com/bosDZkBJxNdwo-dXGZeBkYtfCVnTFq96zqC08UV4dmIccI4YBr5p0CyCE7vmj2w=w9999-h9999';
-	} else if (hour >= 15 && hour < 19) {
+	} else if (hour >= 18 && hour <= 20) {
 		// Dusk
 		headerImage = 'http://lh4.ggpht.com/DCGfFj7ILzkFXXDgCliyTAq-cjKs8eyoTstREjhB2grAzzjYnlelGfpIQ4cEX4c=w9999-h9999';
-	} else if (hour >= 19 || hour >= 0 && hour < 6) {
+	} else if (hour >= 21 || hour >= 0 && hour <= 5) {
 		// Night
 		headerImage = 'http://lh6.ggpht.com/QgqUFGYoAxRkyvbl_5Hq2L6CTsaGXt9kaqrMdSxga-462Uyv2IViGw7OBzDMWNI=w9999-h9999';
 	}
