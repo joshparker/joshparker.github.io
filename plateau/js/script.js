06bec958fedc9800b91028e3682658a3f4577c53
joshparker <josh@joshuamatthewparker.com>
2015-03-15 20:38:48 -0600

    youtube-coffee test
    Invocation test

--- plateau/js/script.js
+++ plateau/js/script.js
@@ -1,11 +1,19 @@
 var youtubeId = "M8YjvHYbZ9w";
-
+var invocation = new XMLHttpRequest();
+var url = "https://www.youtube.com/watch?v=" + youtubeId;
 YoutubeVideo(youtubeId, function(video){
-  console.log(video.title);
-  var webm = video.getSource("video/webm", "medium");
-  console.log("WebM: " + webm.url);
-  var mp4 = video.getSource("video/mp4", "medium");
-  console.log("MP4: " + mp4.url);
+	if (invocation) {
+		invocation.open('GET', url, true);
+		invocation.onreadyStateChange = function() {
+			  console.log(video.title);
+			  var webm = video.getSource("video/webm", "medium");
+			  console.log("WebM: " + webm.url);
+			  var mp4 = video.getSource("video/mp4", "medium");
+			  console.log("MP4: " + mp4.url);
 
-  $("<video controls='controls'/>").attr("src", webm.url).appendTo("body");
+			  $("<video controls='controls'/>").attr("src", webm.url).appendTo("body");
+		}
+		invocation.send();
+	}
+  
 });
