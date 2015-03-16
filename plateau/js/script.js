97458fb25a9f7e87a33fbdd5a53f88964107e20d
joshparker <josh@joshuamatthewparker.com>
2015-03-15 20:34:44 -0600

    youtube-coffee test

---
+++ plateau/js/script.js
@@ -0,0 +1,11 @@
+var youtubeId = "M8YjvHYbZ9w";
+
+YoutubeVideo(youtubeId, function(video){
+  console.log(video.title);
+  var webm = video.getSource("video/webm", "medium");
+  console.log("WebM: " + webm.url);
+  var mp4 = video.getSource("video/mp4", "medium");
+  console.log("MP4: " + mp4.url);
+
+  $("<video controls='controls'/>").attr("src", webm.url).appendTo("body");
+});
